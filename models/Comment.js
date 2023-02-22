fb062e9c9a174dee970bb82e09dcfb8799608486
MauritzMaj <mauritz.majelyne@hotmail.com>
2023-02-21 21:46:34 -0500

    added schema, User and Comment model, and WithAuth

---
+++ models/Comment.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+        },
+    },
+    book_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'book',
+          key: 'id',
+          },
+      },
+    
+    text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+    },
+},
+  {
+    hooks: {
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
