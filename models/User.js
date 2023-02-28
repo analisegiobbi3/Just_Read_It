e35a021dd5de080da6d651d1dbda745310862ed0
Analise <analise.giobbi@gc.com>
2023-02-27 19:22:51 -0500

    fixing some stuff

--- models/User.js
+++ models/User.js
@@ -10,12 +10,6 @@ class User extends Model{
 
 User.init(
     {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
         username: {
             type: DataTypes.STRING,
             allowNull: false,
