fe15f4ad613e4d5980dd60503085139d406246b0
MauritzMaj <mauritz.majelyne@hotmail.com>
2023-03-02 21:54:17 -0500

    cleaned up layout

--- public/js/deleteBook.js
+++ public/js/deleteBook.js
@@ -1,6 +1,6 @@
 const deleteBook = async(event) => {
     event.preventDefault();
-    //need some handlgers here that are based off of button/ inputs in the html
+    //need some handlers here that are based off of button/ inputs in the html
     const id = window.location.toString().split('/')[window.location.toString().split('/').length-1];
 
     const response = await fetch(`/api/books/${id}`, {
