04b020d30f49bca35e79d0cd1f9d88cda30ec113
Analise <analise.giobbi@gc.com>
2023-03-05 14:06:33 -0500

    adding readme and fixing the bug with our login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -41,6 +41,7 @@ router.post('/', async (req, res) =>{
         const userData = await User.create(req.body)
         req.session.save(() => {
             req.session.user_id = userData.id;
+            req.session.username = userData.username;
             req.session.logged_in = true;
             res.status(200).json(userData)
         })
@@ -67,6 +68,7 @@ router.post('/login', async (req, res) => {
 
         req.session.save(() => {
             req.session.user_id = userData.id;
+            req.session.username = userData.username;
             req.session.logged_in = true;
             res.json({ user: userData, message: 'You are now logged into the website'});
         })
